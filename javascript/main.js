88b3174fa7d162488efeb60c6c2806eac1787507
Jack Parsons <jackparse14@hotmail.co.uk>
2024-03-23 16:35:42 +0000

    Added colour generation

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-plusplus */
+const hexCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+function getColour() {
+  let hexString = '#';
+  for (let i = 0; i < 6; i++) {
+    hexString += hexCharacters[Math.floor(Math.random() * hexCharacters.length)];
+  }
+  return hexString;
+}
+
+window.addEventListener('load', () => {
+  const palette = document.getElementById('palette');
+  for (const colour of palette.children) {
+    colour.style.backgroundColor = getColour();
+  }
+});
