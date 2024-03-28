ea903f6416ccf73f2d9ef6729045dfba867019fb
Jack Parsons <jackparse14@hotmail.co.uk>
2024-03-28 20:30:26 +0000

    Added Padlock Open & Closing Mechanism

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,27 @@ window.addEventListener('load', () => {
   const hexCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
   const generateBtn = document.getElementById('generate-btn');
 
+  const lockColourBtns = document.getElementsByClassName('lock-btn');
+
+  function changeLockImage(lockBtn) {
+    const lockImg = lockBtn.firstElementChild;
+    if (!lockImg.stateLocked) {
+      lockImg.src = '/assets/lock.png';
+      lockImg.alt = 'An image of a closed padlock';
+      lockImg.stateLocked = true;
+    } else {
+      lockImg.src = '/assets/open-padlock.png';
+      lockImg.alt = 'An image of an open padlock';
+      lockImg.stateLocked = false;
+    }
+  }
+  for (const lockBtn of lockColourBtns) {
+    lockBtn.firstElementChild.stateLocked = false;
+    lockBtn.addEventListener('click', () => {
+      changeLockImage(lockBtn);
+    });
+  }
+
   function getColour() {
     let hexString = '#';
     for (let i = 0; i < 6; i++) {
@@ -16,7 +37,7 @@ window.addEventListener('load', () => {
     for (const colour of palette.children) {
       const currColour = getColour();
       colour.style.backgroundColor = currColour;
-      colour.textContent = currColour;
+      colour.firstElementChild.innerHTML = currColour;
     }
   }
   createPalette();
