ef6cbb6fdac5c15037d57a8695841caad8a5164f
Jack Parsons <jackparse14@hotmail.co.uk>
2024-03-28 18:01:05 +0000

    Added Hex Strings to colours

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-plusplus */
 const hexCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
 function getColour() {
   let hexString = '#';
   for (let i = 0; i < 6; i++) {
@@ -8,10 +9,15 @@ function getColour() {
   }
   return hexString;
 }
-
-window.addEventListener('load', () => {
+function createPalette() {
   const palette = document.getElementById('palette');
   for (const colour of palette.children) {
-    colour.style.backgroundColor = getColour();
+    const currColour = getColour();
+    colour.style.backgroundColor = currColour;
+    colour.textContent = currColour;
   }
+}
+
+window.addEventListener('load', () => {
+  createPalette();
 });
