ff080f6a67e9f1b27aa7007fff9cf0950c235654
Jack Parsons <jackparse14@hotmail.co.uk>
2024-03-30 18:49:50 +0000

    Added Export Functionality and Styling of Buttons
    Closes #4

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,21 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-plusplus */
+
 window.addEventListener('load', () => {
   const hexCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
   const generateBtn = document.getElementById('generate-btn');
   const saveBtn = document.getElementById('save-btn');
+  const exportBtn = document.getElementById('export-btn');
+  const exportDropdown = document.getElementById('export-dropdown');
+  const exportPopUp = document.getElementById('export-pop-up');
+  const exportText = document.getElementById('main-text');
+  const exportHeading = document.getElementById('heading-text');
+  const exitPopUpBtn = document.getElementById('exit-btn');
+
+  const exportAsCSSBtn = document.getElementById('export-css-btn');
+  const exportAsJSONBtn = document.getElementById('export-json-btn');
+  const exportAsHEXBtn = document.getElementById('export-hex-btn');
 
   const lockColourBtns = document.getElementsByClassName('lock-btn');
   const palette = document.getElementById('palette');
@@ -34,12 +45,33 @@ window.addEventListener('load', () => {
   function savePalette() {
     localStorage.setItem('palette', JSON.stringify(currColours));
   }
-
-  for (const lockBtn of lockColourBtns) {
-    lockBtn.parentElement.isLocked = false;
-    lockBtn.addEventListener('click', () => {
-      changeLockState(lockBtn);
-    });
+  function exportPaletteAsCSS() {
+    exportPopUp.showModal();
+    let cssResult = '';
+    for (let i = 1; i < currColours.length + 1; i++) {
+      cssResult += `--colour-${i}: ${currColours[i - 1]}`;
+      if (i !== currColours.length) { cssResult += ', '; }
+    }
+    exportText.textContent = cssResult;
+    exportHeading.textContent = 'CSS:';
+  }
+  function exportPaletteAsJSON() {
+    const paletteAsJSON = JSON.stringify(currColours);
+    exportPopUp.showModal();
+    exportText.textContent = paletteAsJSON;
+    exportHeading.textContent = 'JSON:';
+  }
+  function exportPaletteAsHEX() {
+    exportPopUp.showModal();
+    exportText.textContent = currColours;
+    exportHeading.textContent = 'HEX:';
+  }
+  function showExportDropdown() {
+    if (exportDropdown.style.display === 'none') {
+      exportDropdown.style.display = 'flex';
+    } else {
+      exportDropdown.style.display = 'none';
+    }
   }
 
   function getColour() {
@@ -62,10 +94,33 @@ window.addEventListener('load', () => {
   }
   createPalette();
 
+  exitPopUpBtn.addEventListener('click', () => {
+    exportPopUp.close();
+  });
+
+  exportAsHEXBtn.addEventListener('click', () => {
+    exportPaletteAsHEX();
+  });
+  exportAsJSONBtn.addEventListener('click', () => {
+    exportPaletteAsJSON();
+  });
+  exportAsCSSBtn.addEventListener('click', () => {
+    exportPaletteAsCSS();
+  });
+
+  for (const lockBtn of lockColourBtns) {
+    lockBtn.parentElement.isLocked = false;
+    lockBtn.addEventListener('click', () => {
+      changeLockState(lockBtn);
+    });
+  }
   generateBtn.addEventListener('click', () => {
     createPalette();
   });
   saveBtn.addEventListener('click', () => {
     savePalette();
+  });
+  exportBtn.addEventListener('click', () => {
+    showExportDropdown();
   });
 });
